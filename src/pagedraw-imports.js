0343b7e11c60fabbb8646557d6c8e3d446ab4fa7
Michael Dwyer <mcdwyer1@gmail.com>
2018-06-07 19:43:02 -0700

    Add bootstrap and new material ui components

--- src/pagedraw-imports.js
+++ src/pagedraw-imports.js
@@ -7,14 +7,41 @@ import {
     IconButton,
     Switch,
     Dialog,
-    TextField
+    TextField,
+    Card,
+    Checkbox,
+    CircularProgress,
+    Input,
+    LinearProgress,
+    Select,
+    Radio
 } from '@material-ui/core';
 
 
+import {
+    Alert,
+    Badge,
+    Button as BsButton,
+    Image,
+    ProgressBar,
+    Tooltip,
+    Glyphicon,
+    Jumbotron,
+    Label,
+    Well,
+    DropdownButton
+} from 'react-bootstrap';
+
 const Enum = (options) => ({__ty: 'Enum', options});
+
+// Material ui types
 const colorTy = Enum(['default', 'primary', 'secondary']);
 const variantTy = Enum(['raised', 'flat', 'outlined', 'fab']);
 const sizeTy = Enum(['small', 'medium', 'large']);
+
+// React bootstrap types
+const bsStyleTy = Enum(['success', 'warning', 'danger', 'info']);
+const bsSizeTy = Enum(["lg", "large", "sm", "small", "xs", "xsmall"])
 
 const expt = pagedrawSpecs([
     {
@@ -49,6 +76,147 @@ const expt = pagedrawSpecs([
                 propTypes: {defaultValue: 'Text', helperText: 'Text', label: 'Text', disabled: 'Boolean', error: 'Boolean', fullWidth: 'Boolean'},
                 resizable: ['width'],
                 importPath: '@material-ui/core',
+                isDefaultExport: false
+            },
+            {
+                name: 'Checkbox',
+                tag: Checkbox,
+                propTypes: {color: colorTy, checked: 'Boolean', disabled: 'Boolean'},
+                resizable: [],
+                importPath: '@material-ui/core',
+                isDefaultExport: false
+            },
+            {
+                name: 'CircularProgress',
+                tag: CircularProgress,
+                propTypes: {color: colorTy, thickness: 'Number', size: 'Number', value: "Number", variant: Enum(['determinate', 'indeterminate', 'static'])},
+                resizable: [],
+                importPath: '@material-ui/core',
+                isDefaultExport: false
+            },
+            {
+                name: 'Input',
+                tag: Input,
+                propTypes: {defaultValue: 'Text', disabled: "Boolean", disableUnderline: "Boolean", error: 'Boolean', multiline: 'Boolean'},
+                resizable: ['width'],
+                importPath: '@material-ui/core',
+                isDefaultExport: false
+            },
+            {
+                name: 'LinearProgress',
+                tag: LinearProgress,
+                propTypes: {value: 'Number', valueBuffer: 'Number', color: Enum(['primary', 'secondary']), variant: Enum(['determinate', 'indeterminate', 'buffer', 'query'])},
+                resizable: ['width'],
+                importPath: '@material-ui/core',
+                isDefaultExport: false
+            },
+            {
+                name: 'Select',
+                tag: Select,
+                propTypes: {value: 'Text', open: 'Boolean', displayEmpty: 'Boolean', children: 'Text'},
+                resizable: ['width'],
+                importPath: '@material-ui/core',
+                isDefaultExport: false
+            },
+            {
+                name: 'Radio',
+                tag: Radio,
+                propTypes: {color: colorTy, disabled: 'Boolean', disableRipple: 'Boolean', checked: 'Boolean', },
+                resizable: [],
+                importPath: '@material-ui/core',
+                isDefaultExport: false
+            },
+        ], 'name')
+    },
+    {
+        name: 'React Bootstrap',
+        children: _.sortBy([
+            {
+                name: 'Alert',
+                tag: Alert,
+                propTypes: {bsStyle: bsStyleTy, children: "Text"},
+                resizable: ['height', 'width'],
+                importPath: 'react-bootstrap',
+                isDefaultExport: false
+            },
+            {
+                name: 'Button',
+                tag: BsButton,
+                propTypes: {bsStyle: bsStyleTy, bsSize: bsSizeTy, type: Enum(['button', 'reset', 'submit']), active: 'Boolean', disabled: 'Boolean', block: 'Boolean', children: "Text"},
+                resizable: ['height', 'width'],
+                importPath: 'react-bootstrap',
+                isDefaultExport: false
+            },
+            {
+                name: 'ProgressBar',
+                tag: ProgressBar,
+                propTypes: {bsStyle: bsStyleTy, min: "Number", now: 'Number', max: "Number", active: "Boolean", striped: 'Boolean'},
+                resizable: ['height', 'width'],
+                importPath: 'react-bootstrap',
+                isDefaultExport: false
+            },
+            {
+                name: 'Tooltip',
+                tag: Tooltip,
+                propTypes: {id: 'Text', placement: Enum(['top', 'right', 'bottom', 'left']), onOpen: 'Function', onClose: 'Function', children: 'Text'},
+                resizable: ['height', 'width'],
+                importPath: 'react-bootstrap',
+                isDefaultExport: false
+            },
+            {
+                name: 'Badge',
+                tag: Badge,
+                propTypes: {pullRight: 'Boolean'},
+                resizable: ['height', 'width'],
+                importPath: 'react-bootstrap',
+                isDefaultExport: false
+            },
+            {
+                name: 'Glyphicon',
+                tag: Glyphicon,
+                propTypes: {glyph: 'Text'},
+                resizable: ['height', 'width'],
+                importPath: 'react-bootstrap',
+                isDefaultExport: false
+            },
+            {
+                name: 'Image',
+                tag: Image,
+                propTypes: {thumbnail: 'Boolean', responsive: 'Boolean', rounded: 'Boolean', circle: 'Boolean'},
+                resizable: ['height', 'width'],
+                importPath: 'react-bootstrap',
+                isDefaultExport: false
+            },
+            {
+                name: 'Jumbotron',
+                tag: Jumbotron,
+                propTypes: {children: 'Text'},
+                resizable: [],
+                importPath: 'react-bootstrap',
+                isDefaultExport: false
+            },
+            {
+                name: 'Label',
+                tag: Label,
+                propTypes: {bsStyle: bsStyleTy, children: 'Text'},
+                resizable: [],
+                importPath: 'react-bootstrap',
+                isDefaultExport: false
+            },
+            {
+                name: 'Well',
+                tag: Well,
+                propTypes: {bsSize: bsSizeTy, children: 'Text'},
+                resizable: [],
+                importPath: 'react-bootstrap',
+                isDefaultExport: false
+            },
+            {
+                name: 'DropdownButton',
+                tag: DropdownButton,
+                propTypes: {bsStyle: bsStyleTy, bsSize: bsSizeTy, title: 'Text', noCaret: 'Boolean', children: 'Text'},
+                resizable: ['height', 'width'],
+                importPath: 'react-bootstrap',
                 isDefaultExport: false
             }
         ], 'name')
